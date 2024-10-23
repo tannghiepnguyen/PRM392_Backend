9c2b5d041cb7a8de269b03a0ef4acc2c63c156a5
psp-xuanquy <xquy118@gmail.com>
2024-10-24 00:03:12 +0700

    update

--- PRM392_Backend.API/Program.cs
+++ PRM392_Backend.API/Program.cs
@@ -1,4 +1,4 @@
-using Microsoft.AspNetCore.Authentication.JwtBearer;
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.OpenApi.Models;
 using PRM392_Backend.API;
 using PRM392_Backend.API.Extensions;
@@ -21,33 +21,33 @@ builder.Services.AddControllers()
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(options =>
 {
-	options.AddSecurityDefinition(JwtBearerDefaults.AuthenticationScheme, new Microsoft.OpenApi.Models.OpenApiSecurityScheme()
-	{
-		Description =
-			"JWT Authorization header using the Bearer scheme. \r\n\r\n" +
-			"Enter 'Bearer' [space] and then your token in the text input below.\r\n\r\n" +
-			"Example: \"Bearer 12345abcdef\"",
-		Name = "Authorization",
-		In = ParameterLocation.Header,
-		Scheme = JwtBearerDefaults.AuthenticationScheme
-	});
-	options.AddSecurityRequirement(new OpenApiSecurityRequirement()
-	{
-		{
-			new OpenApiSecurityScheme()
-			{
-				Reference = new OpenApiReference()
-				{
-					Type = ReferenceType.SecurityScheme,
-					Id = "Bearer"
-				},
-				Scheme = "oath2",
-				Name = "Bearer",
-				In = ParameterLocation.Header
-			},
-			new List<string>()
-		}
-	});
+    options.AddSecurityDefinition(JwtBearerDefaults.AuthenticationScheme, new Microsoft.OpenApi.Models.OpenApiSecurityScheme()
+    {
+        Description =
+            "JWT Authorization header using the Bearer scheme. \r\n\r\n" +
+            "Enter 'Bearer' [space] and then your token in the text input below.\r\n\r\n" +
+            "Example: \"Bearer 12345abcdef\"",
+        Name = "Authorization",
+        In = ParameterLocation.Header,
+        Scheme = JwtBearerDefaults.AuthenticationScheme
+    });
+    options.AddSecurityRequirement(new OpenApiSecurityRequirement()
+    {
+        {
+            new OpenApiSecurityScheme()
+            {
+                Reference = new OpenApiReference()
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                },
+                Scheme = "oath2",
+                Name = "Bearer",
+                In = ParameterLocation.Header
+            },
+            new List<string>()
+        }
+    });
 });
 
 
@@ -66,11 +66,11 @@ var app = builder.Build();
 app.UseExceptionHandler(opt => { });
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+app.UseSwagger();
+app.UseSwaggerUI(c =>
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "PRM392_Backend API v1");
+});
 
 app.UseCors("CorsPolicy");
 
