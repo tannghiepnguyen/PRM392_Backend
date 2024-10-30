c2937834b6a8e2ace5d88076b9525866fd6dc001
Nguyễn Lê Tấn Nghiệp <tannghiepnguyen123@gmail.com>
2024-10-30 11:26:02 +0700

    Add Billing Address Migration

---
+++ PRM392_Backend.Infrastructure/Migrations/20241030042454_AddBillingToOrder.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PRM392_Backend.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddBillingToOrder : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Notifications_AspNetUsers_UserID",
+                table: "Notifications");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Payments_Orders_OrderID",
+                table: "Payments");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Payments_OrderID",
+                table: "Payments");
+
+            migrationBuilder.DropColumn(
+                name: "IsActive",
+                table: "Notifications");
+
+            migrationBuilder.RenameColumn(
+                name: "ID",
+                table: "Notifications",
+                newName: "NotificationID");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "PaymentStatus",
+                table: "Payments",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "OrderID",
+                table: "Payments",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"),
+                oldClrType: typeof(Guid),
+                oldType: "uniqueidentifier",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Amount",
+                table: "Payments",
+                type: "decimal(18,2)",
+                nullable: false,
+                oldClrType: typeof(double),
+                oldType: "float");
+
+            migrationBuilder.AddColumn<string>(
+                name: "BillingAddress",
+                table: "Orders",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "UserID",
+                table: "Notifications",
+                type: "nvarchar(450)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)",
+                oldNullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Payments_OrderID",
+                table: "Payments",
+                column: "OrderID",
+                unique: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Notifications_AspNetUsers_UserID",
+                table: "Notifications",
+                column: "UserID",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Payments_Orders_OrderID",
+                table: "Payments",
+                column: "OrderID",
+                principalTable: "Orders",
+                principalColumn: "ID",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Notifications_AspNetUsers_UserID",
+                table: "Notifications");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Payments_Orders_OrderID",
+                table: "Payments");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Payments_OrderID",
+                table: "Payments");
+
+            migrationBuilder.DropColumn(
+                name: "BillingAddress",
+                table: "Orders");
+
+            migrationBuilder.RenameColumn(
+                name: "NotificationID",
+                table: "Notifications",
+                newName: "ID");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "PaymentStatus",
+                table: "Payments",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(50)",
+                oldMaxLength: 50);
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "OrderID",
+                table: "Payments",
+                type: "uniqueidentifier",
+                nullable: true,
+                oldClrType: typeof(Guid),
+                oldType: "uniqueidentifier");
+
+            migrationBuilder.AlterColumn<double>(
+                name: "Amount",
+                table: "Payments",
+                type: "float",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,2)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "UserID",
+                table: "Notifications",
+                type: "nvarchar(450)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsActive",
+                table: "Notifications",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Payments_OrderID",
+                table: "Payments",
+                column: "OrderID",
+                unique: true,
+                filter: "[OrderID] IS NOT NULL");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Notifications_AspNetUsers_UserID",
+                table: "Notifications",
+                column: "UserID",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Payments_Orders_OrderID",
+                table: "Payments",
+                column: "OrderID",
+                principalTable: "Orders",
+                principalColumn: "ID");
+        }
+    }
+}
